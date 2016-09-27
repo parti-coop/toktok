ac9be677998060225ed983422b30da886d8e4f7e
qus <qus1225@gmail.com>
2016-09-28 02:05:09 +0900

    프로젝트 상세페이지 퍼블(상임위부분까지) #308

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,10 @@ module ApplicationHelper
     date.strftime("%Y.%m.%d %H:%M")
   end
 
+  def static_date_f_not_hour(date)
+    date.strftime("%Y-%m-%d")
+  end
+
   def excerpt(text, options = {})
     options[:length] = 130 unless options.has_key?(:length)
     truncate((strip_tags(text).try(:html_safe)), options)
@@ -15,5 +19,25 @@ module ApplicationHelper
 
   def upload_file_exists?(file_object)
     file_object.try(:file).try(:exists?)
+  end
+
+  def asset_data_base64(path)
+    content, content_type = parse_asset(path)
+    base64 = Base64.encode64(content).gsub(/\s+/, "")
+    "data:#{content_type};base64,#{Rack::Utils.escape(base64)}"
+  end
+
+  def parse_asset(path)
+    if Rails.application.assets
+      asset = Rails.application.assets.find_asset(path)
+      throw "Could not find asset '#{path}'" if asset.nil?
+      return asset.to_s, asset.content_type
+    else
+      name = Rails.application.assets_manifest.assets[path]
+      throw "Could not find asset '#{path}'" if name.nil?
+      content_type = MIME::Types.type_for(name).first.content_type
+      content = open(File.join(Rails.public_path, 'assets', name)).read
+      return content, content_type
+    end
   end
 end
