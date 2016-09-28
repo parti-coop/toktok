7a41bae1eca709b060bf6f0b326b756084a22440
qus <qus1225@gmail.com>
2016-09-28 10:52:04 +0900

    코드정리 - 이미지를 불러올때 image_url메서드를 사용합니다

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,26 +21,6 @@ module ApplicationHelper
     file_object.try(:file).try(:exists?)
   end
 
-  def asset_data_base64(path)
-    content, content_type = parse_asset(path)
-    base64 = Base64.encode64(content).gsub(/\s+/, "")
-    "data:#{content_type};base64,#{Rack::Utils.escape(base64)}"
-  end
-
-  def parse_asset(path)
-    if Rails.application.assets
-      asset = Rails.application.assets.find_asset(path)
-      throw "Could not find asset '#{path}'" if asset.nil?
-      return asset.to_s, asset.content_type
-    else
-      name = Rails.application.assets_manifest.assets[path]
-      throw "Could not find asset '#{path}'" if name.nil?
-      content_type = MIME::Types.type_for(name).first.content_type
-      content = open(File.join(Rails.public_path, 'assets', name)).read
-      return content, content_type
-    end
-  end
-
   def date_f(date)
     timeago_tag date, lang: :ko, limit: 100.days.ago
   end
