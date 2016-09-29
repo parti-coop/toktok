3d930fe6c243088db4d8e6066bf355d625ea5190
qus <qus1225@gmail.com>
2016-09-29 22:09:40 +0900

    소셜공유를 할 수 있습니다. #327

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,8 @@
 //= require jquery.validate
 //= require jquery.waypoints
 //= require waypoints/sticky.js
+//= require jssocials
+//= require kakao
 
 UnobtrusiveFlash.flashOptions['timeout'] = 3000;
 
@@ -23,8 +25,70 @@ function scroll_to_anchor(anchor_id){
     $('html,body').animate({scrollTop: tag.offset().top},'fast');
 }
 
+Kakao.init('6cd2725534444560cb5fe8c77b020bd6');
+
+$.is_blank = function (obj) {
+  if (!obj || $.trim(obj) === "") return true;
+  if (obj.length && obj.length > 0) return false;
+
+  for (var prop in obj) if (obj[prop]) return false;
+  return true;
+}
+
+var prepare_social_share = function($base) {
+  $base.find('[data-action="toktok-share"]').each(function(i, elm) {
+    var $elm = $(elm);
+
+    var url = $elm.data('share-url');
+    var sitename = $elm.data('share-sitename');
+    var text = $elm.data('share-text');
+    var share = $elm.data('share-provider');
+    var logo = $elm.data('share-logo');
+    var css = $elm.data('share-css');
+    if ($.is_blank(share)) return;
+    var image_url = $elm.data('share-image');
+    var image_width = $elm.data('share-image-width');
+    var image_height = $elm.data('share-image-height');
+
+    switch(share) {
+    case 'kakao-link':
+      Kakao.Link.createTalkLinkButton({
+        container: elm,
+        label: text,
+        image: {
+          src: image_url,
+          width: image_width,
+          height: image_height
+        },
+        webLink: {
+          text: sitename + '에서 보기',
+          url: url
+        }
+      });
+    break
+    case 'kakao-story':
+      $elm.on('click', function(e) {
+        Kakao.Story.share({
+          url: url,
+          text: text
+        });
+      });
+    break
+    default:
+      $elm.jsSocials({
+        showCount: false,
+        showLabel: false,
+        shares: [{share: share, logo: logo, css: css}],
+        text: text,
+        url: url
+      });
+    }
+  });
+}
+
 $(function(){
   $('.action-congressman-select').select2();
+  prepare_social_share($('html'));
   $('.action-mention').on('click', function(e) {
     e.preventDefault();
     var $target = $(e.currentTarget);
