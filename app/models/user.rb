d56739bcb7bde3b9e17e41f58dbc9076c4257d17
dali <dalikim@parti.xyz>
2016-10-04 08:13:40 +0900

    가입 시 메일 확인 과정 삭제

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   enumerize :role, in: [:citizen, :staff]
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :confirmable,
+  # :confirmable, :lockable, :timeoutable and :omniauthable, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable,
          :rememberable, :trackable, :omniauthable,
          :omniauth_providers => [:facebook, :twitter]
 
