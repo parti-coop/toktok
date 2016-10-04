3a8e7a2b7a93c223407ac747d53e3a6345952906
qus <qus1225@gmail.com>
2016-10-04 12:55:19 +0900

    오류수정 - 카카오톡 공유 이미지가 깨지는 문제와 페이스북 공유문구에 html태그가 들어가는 문제를 해결합니다 #349

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,6 +7,6 @@ class ProjectsController < ApplicationController
   end
 
   def reset_meta_tags
-    prepare_meta_tags({title: @project.title, description: @project.summary, image: @project.image.url, url: request.original_url})
+    prepare_meta_tags({title: @project.title, description: @project.summary.html_safe, image: @project.image.url, url: request.original_url})
   end
 end
