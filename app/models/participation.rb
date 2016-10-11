b2ee6a9b0094d54d9228e24f879d9327ef082475
qus <qus1225@gmail.com>
2016-10-11 11:11:37 +0900

    오류수정 - 중복클릭을(모델수준에서) 막습니다

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -2,5 +2,7 @@ class Participation < ApplicationRecord
   belongs_to :user
   belongs_to :project, counter_cache: true
 
+  validates :user, uniqueness: {scope: :project_id}
+
   scope :recent, -> { order(created_at: :desc) }
 end
