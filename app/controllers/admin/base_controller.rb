13ab8c02dffe9afff1598153897c038d5b257fd1
qus <qus1225@gmail.com>
2016-10-25 21:05:13 +0900

    관리자는 프로젝트별 참여시민 리스트를 다운로드 할 수 있습니다 #455

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -7,7 +7,12 @@ module Admin
     end
 
     def download_emails
-      @emails = User.all.select { |u| u.email.present? }.map { |u| {email: u.email, name: u.nickname, provider: u.provider} }.uniq { |u| u[:email] }
+      if params[:project_id].present?
+        project = Project.find(params[:project_id])
+        @emails = project.participations.select { |p| p.user.email.present? }.map { |p| {project: p.project.title, email: p.user.email, name: p.user.nickname, provider: p.user.provider} }.uniq { |p| p[:email]}
+      else
+        @emails = User.all.select { |u| u.email.present? }.map { |u| {email: u.email, name: u.nickname, provider: u.provider} }.uniq { |u| u[:email] }
+      end
       respond_to do |format|
         format.xlsx
       end
@@ -18,5 +23,5 @@ module Admin
     def verify_staff
       redirect_to root_url unless current_user.try(:role).try(:staff?)
     end
-  end 
+  end
 end
