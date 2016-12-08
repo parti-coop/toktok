7146f6e32905d9f89ea3e1a4423e278a85b9e59c
qus <qus1225@gmail.com>
2016-12-08 20:54:13 +0900

    프로젝트 관리폼에 제안자 전화번호, 이메일이 추가됩니다

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -40,7 +40,7 @@ module Admin
     private
 
     def project_params
-      params.require(:project).permit(:title, :body, :proposal_id, :participations_goal_count, :image, :image_cache, :summary, :proposer, :proposer_description, :matching_staff_message, :on_running, :running_platform_url, committee_ids: [], attachments_attributes: [ :id, :source, :source_cache, :_destroy ])
+      params.require(:project).permit(:title, :body, :proposal_id, :participations_goal_count, :image, :image_cache, :summary, :proposer, :proposer_phone, :proposer_email, :proposer_description, :matching_staff_message, :on_running, :running_platform_url, committee_ids: [], attachments_attributes: [ :id, :source, :source_cache, :_destroy ])
     end
 
     def should_exists_committees
