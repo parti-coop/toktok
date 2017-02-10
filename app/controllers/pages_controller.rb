5a20def16d30a01ead16bdb7e44e7892e9056623
qus <qus1225@gmail.com>
2017-02-10 11:34:53 +0900

    카테고리 순서가 바뀝니다

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
     @proposals = Proposal.all
     @questions = Question.all
-    @projects = Project.recent.page(params[:page])
+    @projects = Project.hottest.page(params[:page])
     @mainslides = Mainslide.all.priority.sequential
   end
 end
